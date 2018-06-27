c979d545e11f37db00d422f54eb58370bf9ca2fa
hdiaz.nectia <hdiaz@nectia.com>
2018-06-27 14:49:53 -0400

    init repo

---
+++ webpersonal/portfolio/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(null=True, blank=True, verbose_name="Dirección Web")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de Edición")
+
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = [ "-created" ]
+
+    def __str__(self):
+        return self.title
